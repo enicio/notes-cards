84b68136b2cedde43be7ebb4500028634fc495d6
enicio <jessandro42@gmail.com>
2024-02-23 09:06:40 -0300

    feat: add clerk authentication

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,8 @@ import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 import { tursoClient } from "./lib";
+import { SignInButton, SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react"
+
 
 interface Note {
   id: string;
@@ -13,11 +15,12 @@ interface Note {
 export function App() {
   const [search, setSearch] = useState("");
   const [notes, setNotes] = useState<Note[]>([]);
-
+  const { user } = useUser();
   useEffect(() => {
+    if (!user) return;
     const fetchData = async (): Promise<void> => {
       try {
-        const result = await tursoClient.execute("SELECT * FROM frameworks");
+        const result = await tursoClient.execute({sql:"SELECT * FROM frameworks WHERE user_id = ?", args: [user.id]});
         const notes: Note[] = result.rows.map((row): Note => {
           const { id, date_created, note } = row;
           return {
@@ -34,10 +37,12 @@ export function App() {
     };
 
     fetchData();
-  }, []);
+  }, [user]);
 
 
   async function onNoteCreated(content: string) {
+    if (!user) return;
+
     const newNote = {
       id: crypto.randomUUID(),
       date_created: new Date(),
@@ -47,8 +52,8 @@ export function App() {
     const notesArray = [newNote, ...notes];
     try {
       await tursoClient.execute({
-        sql:"INSERT INTO frameworks (date_created, note) VALUES (?, ?)",
-        args: [newNote.date_created, newNote.content]
+        sql:"INSERT INTO frameworks (user_id, date_created, note) VALUES (?, ?, ?)",
+        args: [ user?.id, newNote.date_created, newNote.content]
       });
     } catch (error: any) {
       console.log('Somenthing goes wrong!',error.message);
@@ -58,10 +63,15 @@ export function App() {
 
   }
 
-  async function onNoteDeleted(id: string) {
+  /**
+   * Deletes a note with the specified ID.
+   * @param id - The ID of the note to be deleted.
+   * @returns A promise that resolves when the note is successfully deleted.
+   */
+  async function onNoteDeleted(id: string): Promise<void> {
     try {
       await tursoClient.execute({
-        sql:"DELETE FROM frameworks WHERE id = ?",
+        sql: "DELETE FROM frameworks WHERE id = ?",
         args: [id]
       });
     } catch (error) {
@@ -89,29 +99,40 @@ export function App() {
 
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
-      <img src={logo} alt="NLW Expert" />
-
-      <form className="w-full">
-        <input
-          type="text"
-          placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
-          onChange={handleSearch}
-        />
-      </form>
-
-      <div className="h-px bg-slate-700" />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard onNoteCreated={onNoteCreated} />
-
-        {notes.length !== 0 ? filteredNotes.map((note) => {
-          return (
-            <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
-          );
-        }) : <span>Nenhuma nota encontrada</span>}
+    <>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <div className="bg-gradient-to-r from-transparent from-60% to-slate-500 tra flex justify-end p-2 h-18 items-center">
+        <span className="px-4">{user?.fullName}</span>
+        <UserButton />
       </div>
-    </div>
+      <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+        <img src={logo} alt="NLW Expert" />
+
+        <form className="w-full">
+          <input
+            type="text"
+            placeholder="Busque em suas notas..."
+            className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+            onChange={handleSearch}
+            />
+        </form>
+
+        <div className="h-px bg-slate-700" />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+          <NewNoteCard onNoteCreated={onNoteCreated} />
+
+          {notes.length !== 0 ? filteredNotes.map((note) => {
+            return (
+              <NoteCard onNoteDeleted={onNoteDeleted} key={note.id} note={note} />
+              );
+            }) : <span>Nenhuma nota encontrada</span>}
+        </div>
+      </div>
+    </SignedIn>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
+import { ClerkProvider } from '@clerk/clerk-react'
 
 import "./index.css";
 import { Toaster } from "sonner";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
     <Toaster richColors />
   </React.StrictMode>
 );
